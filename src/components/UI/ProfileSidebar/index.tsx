2de54c6a79d5f7dd855ce2b128305a24b729dbd9
gandib <55914763+gandib@users.noreply.github.com>
2024-10-09 01:26:00 +0600

    follow unfollow completed

--- src/components/UI/ProfileSidebar/index.tsx
+++ src/components/UI/ProfileSidebar/index.tsx
@@ -20,33 +20,7 @@ const ProfileSidebar = () => {
 
   console.log(user);
   return (
-    <div>
-      {/* <div className="rounded-xl bg-default-100 p-2">
-        <div className="h-[100px] w-full rounded-md ">
-          <Image
-            src={user?.data?.image as string}
-            alt="Profile"
-            height={100}
-            width={100}
-            className="rounded-xl"
-          />
-        </div>
-        <div className="my-3">
-          <h1 className="text-2xl font-semibold">{user?.data?.name}</h1>
-          <p className="break-words text-sm">{user?.data?.email}</p>
-          <p className="break-words text-sm py-3 text-justify">
-            {user?.data?.bio}
-          </p>
-        </div>
-        <Button
-          as={Link}
-          className="mt-2 w-full rounded-md"
-          href="/profile/update-profile"
-        >
-          Update Profile
-        </Button>
-      </div> */}
-
+    <div className="fixed">
       <div className="mt-3 space-y-2 min-h-screen rounded-xl bg-default-100 p-2">
         {isLoading && (
           <div className=" bg-black/10 fixed w-1/4 h-14 rounded-md backdrop-blur-md flex justify-center items-center">
