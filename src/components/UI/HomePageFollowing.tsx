26cfdb4e88a23b8054b0a11ca06080176edff748
gandib <55914763+gandib@users.noreply.github.com>
2025-01-11 00:44:18 +0600

    Follower completed

--- src/components/UI/HomePageFollowing.tsx
+++ src/components/UI/HomePageFollowing.tsx
@@ -1,59 +1,100 @@
 "use client";
 
-import { User } from "@nextui-org/react";
+import { useUser } from "@/src/context/user.provider";
+import {
+  useGetUser,
+  useGetUserById,
+  useUpdateFollowing,
+  useUpdateUnfollowing,
+} from "@/src/hooks/user.hook";
+import { Button, User } from "@nextui-org/react";
+import { toast } from "sonner";
 
 const HomePageFollowing = () => {
+  const { user, isLoading } = useUser();
+  const { data } = useGetUser(user?.email!);
+  // const { data: userDataById } = useGetUserById(userId);
+  const { mutate: updateFollower } = useUpdateFollowing(user?.email!);
+  const { mutate: updateUnFollowing } = useUpdateUnfollowing(user?.email!);
+
+  const handleFollowUnfollow = (followUnfollow: string, userId: string) => {
+    if (!user?._id) {
+      return toast("You are not logged in!");
+    }
+    if (followUnfollow === "follow") {
+      const followData = {
+        id: userId,
+        data: {
+          follower: user?._id,
+        },
+      };
+      console.log(followData);
+      updateFollower(followData);
+    } else {
+      const unFollowData = {
+        id: user?._id,
+        data: {
+          following: userId,
+        },
+      };
+      updateUnFollowing(unFollowData);
+    }
+  };
+
+  if (isLoading) {
+    <p>Loading...</p>;
+  }
   return (
     <div className="bg-gray-100 rounded p-6 sticky top-20">
       <h1 className="text-base text-gray-600 font-bold mb-6">
         Who's following
       </h1>
-      <div className="flex text-gray-700 justify-between pb-2">
-        <div className="flex">
-          {/* <Image
-          src={"https://tt.com"}
-          width={40}
-          height={40}
-          alt="img"
-          className="rounded-full bg-primary-500"
-        /> */}
-          {/* <div className="pl-2">
-          <p className="text-sm font-bold">Gandib Roy</p>
-          <p className="text-tiny">Premium</p>
-        </div> */}
-          <User
-            avatarProps={{
-              src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-            }}
-            description="Premium"
-            name="Jane Doe"
-          />
-        </div>
-        <p className="text-xs text-primary-500 cursor-pointer">Follow</p>
-      </div>
-      <div className="flex text-gray-700 justify-between">
-        <div className="flex">
-          {/* <Image
-          src={"https://tt.com"}
-          width={40}
-          height={40}
-          alt="img"
-          className="rounded-full bg-primary-500"
-        />
-        <div className="pl-2">
-          <p className="text-sm font-bold">Gandib Roy</p>
-          <p className="text-tiny">Premium</p>
-        </div> */}
-          <User
-            avatarProps={{
-              src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-            }}
-            description="Premium"
-            name="Jane Doe"
-          />
-        </div>
-        <p className="text-xs text-primary-500 cursor-pointer">Follow</p>
-      </div>
+      {data?.data?.follower?.length < 1 && "No follower found"}
+      {data?.data?.follower?.map(
+        (follower: {
+          _id: string;
+          image: any;
+          membership: string;
+          name: string;
+        }) => (
+          <div
+            key={follower._id}
+            className="flex text-gray-700 justify-between pb-2"
+          >
+            <div className="flex">
+              <User
+                avatarProps={{
+                  src: `${follower.image}`,
+                }}
+                description={follower.membership}
+                name={follower.name}
+              />
+            </div>
+
+            <Button
+              onPress={() =>
+                handleFollowUnfollow(
+                  data?.data?.following?.some(
+                    (following: any) => following._id === follower._id
+                  )
+                    ? "following"
+                    : "follow",
+                  follower._id
+                )
+              }
+              size="sm"
+              variant="light"
+              className="text-green-500 text-sm"
+            >
+              {data?.data?.following?.some(
+                (following: any) => following._id === follower._id
+              )
+                ? "following"
+                : "follow"}
+            </Button>
+          </div>
+        )
+      )}
     </div>
   );
 };
